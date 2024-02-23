9730f9b752c77294322517a6399fdf169897fda1
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-02-23 20:03:29 +0100

    select elements with JS

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+const user = "Jhay";
+const pin = 1212;
 
+const headerElements = document.getElementsByTagName("header");
+const btn_login = document.getElementById("btn");
+const userName = document.getElementById("username");
+const password = document.getElementById("user_pin");
+const mainContainer = document.getElementById("main");
+const user_balance = document.getElementById("accbal");
+const previous_deposit = document.getElementById("prevdepo");
+
+userName.textContent = "Jhay"
+console.log(userName);
+
+function arr(params) {
+ 
+if (userName !== user) {
+ console.log(userName.textContent);
+} else {
+ console.log("Wrong !!!");
+}
+}
+
+arr()
 
 
 
