ebdcc930c11d1c63ab9cbb7c6a8997173e2ff160
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-02-23 09:43:24 +0100

    adjust styles

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const user = "Jhay";
+const pin = 1212;
+
+const headerElements = document.getElementsByTagName("header");
+const btn_login = document.getElementById("#btn");
+const userName = document.getElementById("#username");
+const password = document.getElementById("#user_pin");
+const mainContainer = document.getElementById("#main");
+const user_balance = document.getElementById('#accbal');
+const previous_deposit = document.getElementById("prevdepo");
+
+// btn_login.addEventListener("click", function () {
+//  if (userName.value === user && password.value === pin){
+//   headerElements.style.opacity = 0;
+// mainContainer.style.opacity = 100;
+//  }
+// });
