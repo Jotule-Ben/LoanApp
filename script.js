c26fb0835f192ca11fb06549a4f5a5be60289d88
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-02-23 20:09:32 +0100

    select more elements with JS

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const password = document.getElementById("user_pin");
 const mainContainer = document.getElementById("main");
 const user_balance = document.getElementById("accbal");
 const previous_deposit = document.getElementById("prevdepo");
+const previous_loan = document.getElementById("prevloan");
+const loanTenure = document.getElementById("loan_tenure");
+const acc__type = document.getElementById("acc_type");
+const loanAmount = document.getElementById("loan_amount");
+const applyButton = document.getElementById("button")
 
 userName.textContent = "Jhay"
 console.log(userName);
